1850bfdf43f1e46c33a516e468ff92d80eb35979
Carlsiry <chenkansheng0@163.com>
2019-09-08 22:00:30 +0800

    feat: racer channel function.

--- racer/racer.go
+++ racer/racer.go
@@ -2,24 +2,22 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		winner = a
-	} else {
-		winner = b
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
 	}
-
-	return
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
